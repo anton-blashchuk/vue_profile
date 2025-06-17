5f560d7771fd0a906208f618a5be049ec9ac54aa
Anton Blashchuk <anton.blashchuk@gmail.com>
2025-06-17 15:18:35 +0200

    removed some of generated code; partially implemented show/edit text input component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,17 @@
 <script setup lang="ts">
+import TextLabelOrInput from './TextLabelOrInput.vue'
+
+
 defineProps<{
   msg: string
 }>()
+
 </script>
 
 <template>
   <div class="greetings">
+    <TextLabelOrInput name="Navn"/>
     <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
-    </h3>
   </div>
 </template>
 
---
+++ src/components/TextLabelOrInput.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+const props = withDefaults(defineProps<{
+    name: string,
+    isEdit: boolean
+}>(),
+    {
+        isEdit: false
+    })
+
+</script>
+<script lang="ts">
+import { ref } from 'vue'
+const text_value = ref("")
+
+
+export default {
+    data() {
+        return {
+            text_value
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="flex-input">
+        <div class="label">{{ name }}</div>
+        <input v-model=text_value></input>
+    </div>
+</template>
+
+<style scoped>
+button {
+    font-weight: bold;
+}
+
+.flex-input {
+    display: flex;
+}
+
+.label {
+    padding-right: 6pt;
+}
+</style>
